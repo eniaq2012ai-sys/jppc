7c144236baf17e898e3b5035c90195d6d9661d70
eniaq2012ai-sys <eniaq2012ai@gmail.com>
2026-09-09 14:09:32 +0700

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyA3PtCN8kNfx7MjyxMQHBz5PS2fPOLxzTo",
-  authDomain: "jppc-cea43.firebaseapp.com",
-  projectId: "jppc-cea43",
-  storageBucket: "jppc-cea43.firebasestorage.app",
-  messagingSenderId: "1093206364719",
-  appId: "1:1093206364719:web:775dc14ec75ede11ebacda"
-};
